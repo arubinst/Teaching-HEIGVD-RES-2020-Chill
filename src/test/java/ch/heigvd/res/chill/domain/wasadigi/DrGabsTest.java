5ff8ba46441826fbfc012862cc788100da5fb49d
Haxos <a.mayocartes@protonmail.com>
2020-02-27 23:05:38 +0100

    adding dr gab's

---
+++ src/test/java/ch/heigvd/res/chill/domain/wasadigi/DrGabsTest.java
@@ -0,0 +1,31 @@
+package ch.heigvd.res.chill.domain.wasadigi;
+
+import ch.heigvd.res.chill.domain.Bartender;
+import ch.heigvd.res.chill.protocol.OrderRequest;
+import ch.heigvd.res.chill.protocol.OrderResponse;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DrGabsTest {
+
+    @Test
+    void thePriceAndNameForDrGabsShouldBeCorrect() {
+        DrGabs beer = new DrGabs();
+        assertEquals(beer.getName(), DrGabs.NAME);
+        assertEquals(beer.getPrice(), DrGabs.PRICE);
+    }
+
+    @Test
+    void aBartenderShouldAcceptAnOrderForDrGabs() {
+        Bartender jane = new Bartender();
+        String productName = "ch.heigvd.res.chill.domain.wasadigi.DrGabs";
+        OrderRequest request = new OrderRequest(3, productName);
+        OrderResponse response = jane.order(request);
+        BigDecimal expectedTotalPrice = DrGabs.PRICE.multiply(new BigDecimal(3));
+        assertEquals(expectedTotalPrice, response.getTotalPrice());
+    }
+
+}
